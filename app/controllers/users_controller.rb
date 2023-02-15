30debb3b64b2b657c6d0983aa53d2cb2c2e757b6
hoangbooth <hoang@chicagobooth.edu>
2023-02-15 18:18:20 +0000

    user and user detail

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+
+  def index
+    
+    matching_users = User.all
+    @list_of_users = matching_users.order({ :username => :asc })
+    render ({ :template => "user_templates/index.html.erb"})
+  end
+
+  def show
+    # Parameters ("path_username")
+  
+    url_username = params.fetch("path_username")
+    matching_usernames = User.where({:username => url_username })
+    
+    @the_user = matching_usernames.first
+
+    # if the_user == nil
+    #   redirect_to("/users", status: 404)
+    # else
+      render({ :template => "user_templates/show.html.erb"})
+    # end
+
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get("/", { :controller => "users", :action => "index"})
+
+  get("/users", { :controller => "users", :action => "index"})
+
+  get("/users/:path_username", { :controller => "users", :action => "show"})
 
 
 end
