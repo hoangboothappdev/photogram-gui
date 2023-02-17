4901c94be28c1608d1773c97525e99505d199a4c
hoangbooth <hoang@chicagobooth.edu>
2023-02-17 07:46:55 +0000

    delete photo done

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -21,4 +21,22 @@ class PhotosController < ApplicationController
   end
 
 
+#delete photos # get("/delete_photo/:path_id", { :controller => "photos", :action => "delete"})
+
+  def delete
+  
+    url_id = params.fetch("path_id")
+    matching_photos = Photo.where({ :id => url_id })
+    @the_photo = matching_photos.at(0)
+
+    @the_photo.destroy
+
+   # render({ :template => "photo_templates/baii.html.erb"})
+    redirect_to("/photos") 
+
+  end
+
+
+
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,4 +23,5 @@ class UsersController < ApplicationController
 
   end
   
+  
 end
